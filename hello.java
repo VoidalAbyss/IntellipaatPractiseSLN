b65f6af1895307a84efd565676fc78c8d22f4b9d
Baba Yaga <johnvick4@march.com>
2023-03-18 16:34:15 +0000

    Made changes feat-101

--- hello.java
+++ hello.java
@@ -3,6 +3,6 @@ class hello
 Public static void main(String args[])
 {
 System.out.println("hello from the other side");
-System.out.println("adele");
+System.out.println("hello from feature 101");
 }
 }
