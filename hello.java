ed74761372710f2f6eeded8e1decab9bc2f74887
Baba Yaga <johnvick4@march.com>
2023-03-19 15:54:48 +0000

    made changes in child branch by mistake, instead of master branch

--- hello.java
+++ hello.java
@@ -3,6 +3,6 @@ class hello
 Public static void main(String args[])
 {
 System.out.println("hello from the other side");
-System.out.println("hello from feature 101");
+System.out.println("hello from master 2.0");
 }
 }
